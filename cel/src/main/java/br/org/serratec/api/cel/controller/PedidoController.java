d2f22dce9e0d774f100db9c438d2f84f49fc275a
Luciano Costa <lucianocosta21@hotmail.com>
2024-05-22 20:30:22 -0300

    rel produto e itemproduto

--- cel/src/main/java/br/org/serratec/api/cel/controller/PedidoController.java
+++ cel/src/main/java/br/org/serratec/api/cel/controller/PedidoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,11 @@ public class PedidoController {
 	@GetMapping
 	public List<Pedido> obterTodos() {
 		return servico.obterTodos();
+	}
+	
+	@PostMapping
+	public Pedido cadastrarPedido(@RequestBody Pedido pedido){
+		return servico.cadastrarPedido(pedido);
 	}
 	
 }
