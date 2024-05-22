ac4806c9c4ed5a20425daa2c164a3800cec6779c
wierdest <andrlzpt@protonmail.com>
2024-05-22 11:02:55 -0300

    Primeiro commit projeto vazio

---
+++ cel/src/main/java/br/org/serratec/api/cel/CelApplication.java
@@ -0,0 +1,13 @@
+package br.org.serratec.api.cel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelApplication.class, args);
+	}
+
+}
---
+++ cel/src/test/java/br/org/serratec/api/cel/CelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.api.cel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
