36216785f132a7217af8401f83218b2e335a46d1
Luciano Costa <lucianocosta21@hotmail.com>
2024-05-22 15:27:37 -0300

    entity pedido

--- cel/src/main/java/br/org/serratec/api/cel/controller/PedidoController.java
+++ cel/src/main/java/br/org/serratec/api/cel/controller/PedidoController.java
@@ -1,5 +1,7 @@
 package br.org.serratec.api.cel.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.org.serratec.api.cel.dtos.ViaCEPDTO;
+import br.org.serratec.api.cel.model.Pedido;
 import br.org.serratec.api.cel.service.PedidoService;
 
 @RestController
@@ -19,6 +22,11 @@ public class PedidoController {
 	@GetMapping("/teste-cep/{cep}")
 	public ViaCEPDTO conferirDadosViaCep(@PathVariable String cep) {
 		return servico.conferirCep(cep);
+	}
+	
+	@GetMapping
+	public List<Pedido> obterTodos() {
+		return servico.obterTodos();
 	}
 	
 }
