57cc950a492672ac6197ab709a24f3ebea79d898
Karensc0 <162759089+Karensc0@users.noreply.github.com>
2024-05-24 21:14:11 -0300

    Anotações dtos

--- cel/src/main/java/br/org/serratec/api/cel/dtos/ViaCEPDTO.java
+++ cel/src/main/java/br/org/serratec/api/cel/dtos/ViaCEPDTO.java
@@ -4,15 +4,26 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import br.org.serratec.api.cel.config.Mapper;
 import br.org.serratec.api.cel.model.Endereco;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ViaCEPDTO(
+		@NotBlank(message = "Digite um CEP válido de 8 dígitos.")
+	    @Pattern(regexp = "\\d{8}")
 		String cep,
+		@NotBlank(message = "É necessário preencher o logradouro.")
 		String logradouro,
+		@NotBlank(message = "Complemento obrigatório.")
 		String complemento,
+		@NotBlank(message = "O bairro precisa ser informado.")
 		String bairro,
+		@NotBlank(message = "O cidade precisa ser informada.")
 		String localidade,
+		@NotBlank(message = "A UF precisa ser informada.")
 		String uf,
+		@NotBlank
+		@Pattern(regexp = "\\d{2}", message = "O DDD deve conter exatamente 2 dígitos")
 		String ddd
 		
 		) {
